5871b6efbc34204c132a205d6dc523ac042a242e
Aditya Chintha <adityachintha@gmail.com>
2024-11-08 15:20:44 -0500

    updated packages

--- app/config/env.js
+++ app/config/env.js
@@ -1,5 +1,5 @@
 const config = {
-  host: "http://10.0.0.140:9000", // Update with the correct IP if needed
+  host: "http://10.0.0.92:9000", // Update with the correct IP if needed
 };
 
 export default config;
