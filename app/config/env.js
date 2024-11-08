c8f9e24cf101c4c1d2c6dbc06fb79f1d1b103c26
Abhirami007 <psabhirami015@gmail..com>
2024-11-08 16:57:46 -0500

    updating live host

--- app/config/env.js
+++ app/config/env.js
@@ -1,5 +1,6 @@
 const config = {
-  host: "http://10.0.0.92:9000", // Update with the correct IP if needed
+  // host: "http://10.0.0.92:9000", // Update with the correct IP if needed
+   host: "https://drivemeappbackend.onrender.com"
 };
 
 export default config;
